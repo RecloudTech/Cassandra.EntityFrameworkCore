205436681dd1704718a37fe4b66e4843dd13a53c
terentev.a.a <terentev.a.a@recloud.tech>
2024-11-03 01:30:56 +0300

    Add CassandraTransactionManager with basic transaction stubs
    Introduced CassandraTransactionManager class implementing transaction-related interfaces with methods that throw NotSupportedException. Added unit tests in CassandraTransactionManagerTests to ensure that all transaction operations are not supported.

---
+++ Cassandra.EntityFramework.Tests/Storage/CassandraTransactionManagerTests.cs
@@ -0,0 +1,33 @@
+﻿using Cassandra.EntityFrameworkCore.Storage;
+
+namespace Cassandra.EntityFramework.Tests.Storage;
+
+public class CassandraTransactionManagerTests
+{
+    [Fact]
+    public async Task Does_not_support_transactions()
+    {
+        var transactionManager = new CassandraTransactionManager();
+
+        Assert.Throws<NotSupportedException>(() => transactionManager.BeginTransaction());
+        await Assert.ThrowsAsync<NotSupportedException>(async () => await transactionManager.BeginTransactionAsync());
+
+        Assert.Throws<NotSupportedException>(() => transactionManager.CommitTransaction());
+        await Assert.ThrowsAsync<NotSupportedException>(async () => await transactionManager.CommitTransactionAsync());
+
+        Assert.Throws<NotSupportedException>(() => transactionManager.RollbackTransaction());
+        await Assert.ThrowsAsync<NotSupportedException>(async () =>
+            await transactionManager.RollbackTransactionAsync());
+
+        Assert.Null(transactionManager.CurrentTransaction);
+        Assert.Null(transactionManager.EnlistedTransaction);
+
+        Assert.Throws<NotSupportedException>(() => transactionManager.EnlistTransaction(null));
+
+        transactionManager.ResetState();
+        await transactionManager.ResetStateAsync();
+
+        Assert.Null(transactionManager.CurrentTransaction);
+        Assert.Null(transactionManager.EnlistedTransaction);
+    }
+}
---
+++ src/Cassandra.EntityFrameworkCore/Storage/CassandraTransactionManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Transactions;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Cassandra.EntityFrameworkCore.Storage;
+
+public class CassandraTransactionManager : IDbContextTransactionManager, ITransactionEnlistmentManager
+{
+    /// <inheritdoc />
+    public void ResetState()
+    {
+    }
+
+    /// <inheritdoc />
+    public Task ResetStateAsync(CancellationToken cancellationToken = new())
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public IDbContextTransaction BeginTransaction()
+    {
+        throw CreateNotSupportedException();
+    }
+
+    /// <inheritdoc />
+    public Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken = new())
+    {
+        throw CreateNotSupportedException();
+    }
+
+    /// <inheritdoc />
+    public void CommitTransaction()
+    {
+        throw CreateNotSupportedException();
+    }
+
+    /// <inheritdoc />
+    public Task CommitTransactionAsync(CancellationToken cancellationToken = new())
+    {
+        throw CreateNotSupportedException();
+    }
+
+    /// <inheritdoc />
+    public void RollbackTransaction()
+    {
+        throw CreateNotSupportedException();
+    }
+
+    /// <inheritdoc />
+    public Task RollbackTransactionAsync(CancellationToken cancellationToken = new())
+    {
+        throw CreateNotSupportedException();
+    }
+
+    /// <inheritdoc />
+    public IDbContextTransaction? CurrentTransaction
+        => null;
+
+    /// <inheritdoc />
+    public void EnlistTransaction(Transaction? transaction)
+    {
+        throw CreateNotSupportedException();
+    }
+
+    /// <inheritdoc />
+    public Transaction? EnlistedTransaction
+        => null;
+
+    private static NotSupportedException CreateNotSupportedException()
+    {
+        return new NotSupportedException("The MongoDB EF Core Provider does not support transactions.");
+    }
+}
